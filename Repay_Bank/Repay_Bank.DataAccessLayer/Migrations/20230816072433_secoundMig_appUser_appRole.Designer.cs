8459a320d928964980bcb35812c8fbcbe744b026
muhammetyild1z <muhammety.yildizz@gmail.com>
2023-08-16 10:47:34 +0300

    .

--- Repay_Bank/Repay_Bank.DataAccessLayer/Migrations/20230816072433_secoundMig_appUser_appRole.Designer.cs
+++ Repay_Bank/Repay_Bank.DataAccessLayer/Migrations/20230816072433_secoundMig_appUser_appRole.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repay_Bank.DataAccessLayer.Concrete;
 
@@ -11,9 +12,10 @@ using Repay_Bank.DataAccessLayer.Concrete;
 namespace Repay_Bank.DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230816072433_secoundMig_appUser_appRole")]
+    partial class secoundMig_appUser_appRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
