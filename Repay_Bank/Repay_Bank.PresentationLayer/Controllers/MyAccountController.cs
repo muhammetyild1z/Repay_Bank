0392e6838924de74821eb190f3e4a042a33faf7a
muhammetyild1z <muhammety.yildizz@gmail.com>
2023-08-28 14:48:11 +0300

    .

---
+++ Repay_Bank/Repay_Bank.PresentationLayer/Controllers/MyAccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Repay_Bank.PresentationLayer.Controllers
+{
+    public class MyAccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
